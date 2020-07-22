65c1299932bb3cfd7a2761a5947923fdcdc8e000
ashish.jain <ashish.jain@HIRING.PUBLICISSAPIENT.COM>
2020-07-22 21:36:50 +0530

    commit

--- src/main/java/com/example/football/business/Processor.java
+++ src/main/java/com/example/football/business/Processor.java
@@ -19,12 +19,8 @@ public class Processor {
 
     public List<Node> getStandings(){
         List<Node> nodes = new ArrayList<>();
-        HashMap<String, String> countryMap = new HashMap<>();
-        for (Country c:rc.getCountries()) {
-            countryMap.put(c.getCountry_name(),c.getCountry_id());
-        }
-
-        Set<String> leagues = Arrays.stream(rc.getCompetitions()).map(i -> i.getLeague_id()).collect(Collectors.toSet());
+        Map<String, String> countryMap = generateCountryMap();
+        Set<String> leagues = getLeagueSet();
         leagues.stream().map( i -> rc.getStanding(i)).flatMap(i -> Arrays.stream(i)).
                 forEach(i -> {
             nodes.add(new Node(country, countryMap.get(i.getCountry_name()) +"-"+i.getCountry_name()));
@@ -33,6 +29,33 @@ public class Processor {
             nodes.add(new Node(position, i.getOverall_league_position()));
         });
         return nodes;
+    }
+
+    public List<Node> searchWithName(String country, String league, String team) {
+        List<Node> nodes = new ArrayList<>();
+        Map<String, String> countryMap = generateCountryMap();
+        Set<String> leagues = getLeagueSet();
+        leagues.stream().map( i -> rc.getStanding(i)).flatMap(i -> Arrays.stream(i)).filter(i ->
+            i.getCountry_name().equals(country) && i.getTeam_name().equals(team) && i.getLeague_name().equals(league)).
+                forEach(i -> {
+                    nodes.add(new Node(country, countryMap.get(i.getCountry_name()) +"-"+i.getCountry_name()));
+                    nodes.add(new Node(league, i.getLeague_id() +"-"+i.getLeague_name()));
+                    nodes.add(new Node(team, i.getTeam_id() +"-"+i.getTeam_name()));
+                    nodes.add(new Node(position, i.getOverall_league_position()));
+                });
+        return nodes;
+    }
+
+    private Set<String> getLeagueSet() {
+        return Arrays.stream(rc.getCompetitions()).map(i -> i.getLeague_id()).collect(Collectors.toSet());
+    }
+
+    private Map<String,String> generateCountryMap(){
+        HashMap<String, String> countryMap = new HashMap<>();
+        for (Country c:rc.getCountries()) {
+            countryMap.put(c.getCountry_name(),c.getCountry_id());
+        }
+        return countryMap;
     }
 
     public static class Node{
--- src/main/java/com/example/football/service/FootballService.java
+++ src/main/java/com/example/football/service/FootballService.java
@@ -1,14 +1,18 @@
 package com.example.football.service;
 
 import com.example.football.business.Processor;
+import com.example.football.model.NameSearchCriteria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.print.attribute.standard.Media;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Path("ranking")
 @Service
@@ -20,6 +24,15 @@ public class FootballService {
     @Produces(MediaType.APPLICATION_JSON)
     public Response get(){
         return Response.status(200).entity(p.getStandings()).build();
+    }
+
+    @GET
+    @Path("{country}/{league}/{team}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getByNameSearch(@PathParam("country") String country, @PathParam("league") String league,
+                                    @PathParam("team") String team){
+        List<Processor.Node> nodes = p.searchWithName(country,league,team);
+        return Response.status(200).entity(nodes).build();
     }
 
 }
